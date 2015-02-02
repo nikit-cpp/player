df121a3f4e5289cd327d060cf40a580f9182cc81
nik <nikit.cpp@yandex.ru>
2015-02-02 21:51:35 +0300

    2 независимых активити: main(со списком) и активность воспроизведения

---
+++ src/com/github/nikit/cpp/player/PlaybackActivity.java
@@ -0,0 +1,220 @@
+package com.github.nikit.cpp.player;
+
+import android.app.Activity;
+import android.content.ContentUris;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.SeekBar;
+
+import java.io.IOException;
+
+import static android.view.View.OnTouchListener;
+
+public class PlaybackActivity extends Activity implements OnPreparedListener,
+        OnCompletionListener {
+
+    final String LOG_TAG = "myLogs";
+
+    final String DATA_HTTP = "http://dl.dropboxusercontent.com/u/6197740/explosion.mp3";
+    final String DATA_STREAM = "http://online.radiorecord.ru:8101/rr_128";
+    final String DATA_SD = Environment
+            .getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)
+            + "/music.mp3";
+    final Uri DATA_URI = ContentUris
+            .withAppendedId(
+                    android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                    13359);
+
+    MediaPlayer mediaPlayer;
+    AudioManager am;
+    CheckBox chbLoop;
+    SeekBar seekBar;
+    private final Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.playback_activity);
+        am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        chbLoop = (CheckBox) findViewById(R.id.chbLoop);
+        chbLoop.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+                if (mediaPlayer != null)
+                    mediaPlayer.setLooping(isChecked);
+            }
+        });
+
+
+        // // mediaPlayer.getCurrentPosition();
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        seekBar.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                seekChange(v);
+                return false;
+            }
+        });
+    }
+
+    // //
+    public void startPlayProgressUpdater() {
+        seekBar.setProgress(mediaPlayer.getCurrentPosition());
+
+        if (mediaPlayer.isPlaying()) {
+            Runnable notification = new Runnable() {
+                public void run() {
+                    startPlayProgressUpdater();
+                }
+            };
+            handler.postDelayed(notification, 1000);
+        } else {
+            mediaPlayer.pause();
+            // buttonPlayStop.setText(getString(R.string.play_str));
+            seekBar.setProgress(0);
+        }
+    }
+
+    // //
+    private void seekChange(View v) {
+        if (mediaPlayer.isPlaying()) {
+            SeekBar sb = (SeekBar) v;
+            mediaPlayer.seekTo(sb.getProgress());
+        }
+    }
+
+    public void onClickStart(View view) {
+        releaseMP();
+
+        try {
+            switch (view.getId()) {
+                case R.id.btnStartHttp:
+                    Log.d(LOG_TAG, "start HTTP");
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(DATA_HTTP);
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    Log.d(LOG_TAG, "prepareAsync");
+                    mediaPlayer.setOnPreparedListener(this);
+                    mediaPlayer.prepareAsync();
+                    break;
+                case R.id.btnStartStream:
+                    Log.d(LOG_TAG, "start Stream");
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(DATA_STREAM);
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    Log.d(LOG_TAG, "prepareAsync");
+                    mediaPlayer.setOnPreparedListener(this);
+                    mediaPlayer.prepareAsync();
+                    break;
+                case R.id.btnStartSD:
+                    Log.d(LOG_TAG, "start SD");
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(DATA_SD);
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    break;
+                case R.id.btnStartUri:
+                    Log.d(LOG_TAG, "start Uri");
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(this, DATA_URI);
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    break;
+                case R.id.btnStartRaw:
+                    Log.d(LOG_TAG, "start Raw");
+                    mediaPlayer = MediaPlayer.create(this, R.raw.explosion);
+                    mediaPlayer.start();
+                    break;
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (mediaPlayer == null)
+            return;
+
+        mediaPlayer.setLooping(chbLoop.isChecked());
+        mediaPlayer.setOnCompletionListener(this);
+
+        seekBar.setMax(mediaPlayer.getDuration());
+        startPlayProgressUpdater();
+    }
+
+    private void releaseMP() {
+        if (mediaPlayer != null) {
+            try {
+                mediaPlayer.release();
+                mediaPlayer = null;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void onClick(View view) {
+        if (mediaPlayer == null)
+            return;
+        switch (view.getId()) {
+            case R.id.btnPause:
+                if (mediaPlayer.isPlaying())
+                    mediaPlayer.pause();
+                break;
+            case R.id.btnResume:
+                if (!mediaPlayer.isPlaying())
+                    mediaPlayer.start();
+                break;
+            case R.id.btnStop:
+                mediaPlayer.stop();
+                break;
+            case R.id.btnBackward:
+                mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() - 3000);
+                break;
+            case R.id.btnForward:
+                mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() + 3000);
+                break;
+            case R.id.btnInfo:
+                Log.d(LOG_TAG, "Playing " + mediaPlayer.isPlaying());
+                Log.d(LOG_TAG, "Time " + mediaPlayer.getCurrentPosition() + " / "
+                        + mediaPlayer.getDuration());
+                Log.d(LOG_TAG, "Looping " + mediaPlayer.isLooping());
+                Log.d(LOG_TAG,
+                        "Volume " + am.getStreamVolume(AudioManager.STREAM_MUSIC));
+                break;
+
+        }
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        Log.d(LOG_TAG, "onPrepared");
+        mp.start();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        Log.d(LOG_TAG, "onCompletion");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releaseMP();
+    }
+}
