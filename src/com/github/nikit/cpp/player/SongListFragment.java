173a4b2f47aec05758d92828cc85cf609abf5499
nik <nikit.cpp@yandex.ru>
2015-02-15 09:36:59 +0300

    не работающий фильтр

--- src/com/github/nikit/cpp/player/SongListFragment.java
+++ src/com/github/nikit/cpp/player/SongListFragment.java
@@ -1,5 +1,6 @@
 package com.github.nikit.cpp.player;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
@@ -9,10 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.*;
 
 import java.util.ArrayList;
 
@@ -49,7 +47,7 @@ public class SongListFragment extends ListFragment {
         getActivity().setTitle(R.string.app_name);
 
         mSongs = SongFabric.get(getActivity()) .getSongs();
-        adapter = new SongAdapter(mSongs);
+        adapter = new SongAdapter(getActivity().getApplicationContext(), R.layout.list_item_song, mSongs);
         setListAdapter(adapter);
     }
 
@@ -83,12 +81,8 @@ public class SongListFragment extends ListFragment {
         startActivity(i);
     }
 
-    private class SongAdapter extends ArrayAdapter<Song> {
+    /*private class SongAdapter extends ArrayAdapter<Song> {
         public SongAdapter(ArrayList<Song> songs){
-            /**
-             * Мы не будем использовать предопределенный макет, поэтому вместо идентифика-
-             тора макета можно передать 0.
-             */
             super(getActivity(), 0, songs);
         }
 
@@ -111,6 +105,139 @@ public class SongListFragment extends ListFragment {
 
             return convertView;
         }
+    }*/
+
+    private class SongAdapter extends ArrayAdapter<Song> {
+
+        private ArrayList<Song> originalList;
+        private ArrayList<Song> countryList;
+        private CountryFilter filter;
+
+        public SongAdapter(Context context, int textViewResourceId,
+                               ArrayList<Song> countryList) {
+            super(context, textViewResourceId, countryList);
+            this.countryList = new ArrayList<Song>();
+            this.countryList.addAll(countryList);
+            this.originalList = new ArrayList<Song>();
+            this.originalList.addAll(countryList);
+        }
+
+        @Override
+        public Filter getFilter() {
+            if (filter == null){
+                filter  = new CountryFilter();
+            }
+            return filter;
+        }
+
+
+        private class ViewHolder {
+            TextView code;
+            TextView name;
+            TextView continent;
+            TextView region;
+        }
+
+        /*@Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+            Log.v("ConvertView", String.valueOf(position));
+            if (convertView == null) {
+
+                LayoutInflater vi = (LayoutInflater)getSystemService(
+                        Context.LAYOUT_INFLATER_SERVICE);
+                convertView = vi.inflate(R.layout.country_info, null);
+
+                holder = new ViewHolder();
+                holder.code = (TextView) convertView.findViewById(R.id.code);
+                holder.name = (TextView) convertView.findViewById(R.id.name);
+                holder.continent = (TextView) convertView.findViewById(R.id.continent);
+                holder.region = (TextView) convertView.findViewById(R.id.region);
+
+                convertView.setTag(holder);
+
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            Country country = countryList.get(position);
+            holder.code.setText(country.getCode());
+            holder.name.setText(country.getName());
+            holder.continent.setText(country.getContinent());
+            holder.region.setText(country.getRegion());
+
+            return convertView;
+
+        }*/
+        public View getView(int position, View convertView, ViewGroup parent){
+            // Если мы не получили представление, заполняем его
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater()
+                        .inflate(R.layout.list_item_song, null);
+            }
+
+            // Настройка представления для объекта Song
+            Song c = getItem(position);
+            TextView nameTextView =
+                    (TextView)convertView.findViewById(R.id.song_list_item_nameTextView);
+            nameTextView.setText(c.getName());
+
+            TextView artistTextView =
+                    (TextView)convertView.findViewById(R.id.song_list_item_artistTextView);
+            artistTextView.setText(c.getArtist());
+
+            return convertView;
+        }
+
+
+        private class CountryFilter extends Filter
+        {
+
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+
+                constraint = constraint.toString().toLowerCase();
+                FilterResults result = new FilterResults();
+                if(constraint != null && constraint.toString().length() > 0)
+                {
+                    ArrayList<Song> filteredItems = new ArrayList<Song>();
+
+                    for(int i = 0, l = originalList.size(); i < l; i++)
+                    {
+                        Song country = originalList.get(i);
+                        if(country.toString().toLowerCase().contains(constraint))
+                            filteredItems.add(country);
+                    }
+                    result.count = filteredItems.size();
+                    result.values = filteredItems;
+                }
+                else
+                {
+                    synchronized(this)
+                    {
+                        result.values = originalList;
+                        result.count = originalList.size();
+                    }
+                }
+                return result;
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void publishResults(CharSequence constraint,
+                                          FilterResults results) {
+
+                countryList = (ArrayList<Song>)results.values;
+                notifyDataSetChanged();
+                clear();
+                for(int i = 0, l = countryList.size(); i < l; i++)
+                    add(countryList.get(i));
+                notifyDataSetInvalidated();
+            }
+        }
+
+
     }
 
     /**
