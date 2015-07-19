5cee89268b2a9732bfcaa26ac7abdcf8d99c97b7
nik <nikit.cpp@yandex.ru>
2015-07-19 03:00:16 +0300

    will trouble on wrong extend

--- src/com/github/nikit/cpp/player/PlayListsActivity.java
+++ src/com/github/nikit/cpp/player/PlayListsActivity.java
@@ -2,12 +2,13 @@ package com.github.nikit.cpp.player;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 /**
  * Активность, содпржащая список плейлистов
  * Created by nik on 14.07.15.
  */
-public class PlayListsActivity extends Activity {
+public class PlayListsActivity extends FragmentActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
